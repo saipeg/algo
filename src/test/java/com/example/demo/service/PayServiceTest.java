d4eb7720f19d2255eb7aac9e258b0502e73c960e
S Andrey <Sogomonyan.A.A@sbertech.ru>
2025-05-28 15:31:59 +0300

    some service

--- src/test/java/com/example/demo/service/PayServiceTest.java
+++ src/test/java/com/example/demo/service/PayServiceTest.java
@@ -3,10 +3,9 @@ package com.example.demo.service;
 import com.example.demo.common.Currency;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class PayServiceTest {
