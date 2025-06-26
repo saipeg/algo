21325b852d7a3ba79990e06c7909ce7e0c5cb964
Andrew S <saipeg@yandex.ru>
2025-06-26 11:54:46 +0300

    Pom up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,46 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Serializable;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	static class Parent implements Cloneable {
+
+	}
+
+	static class Singleton extends Parent implements Serializable {
+
+		private static Singleton INSTANCE = new Singleton();
+		private Singleton() {
+			initializeInstance();
+		}
+
+
+		// эмуляция долгой инициализации
+		private void initializeInstance() {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		/*
+        метод может вызваться из многих потоков одновременно
+        */
+		synchronized public static Singleton getInstance() {
+			if(INSTANCE == null ) {
+				INSTANCE = new Singleton();
+			}
+			return INSTANCE;
+		}
 	}
 
 }
