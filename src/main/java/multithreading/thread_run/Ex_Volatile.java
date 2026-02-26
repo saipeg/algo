b28900154b9408e89a7b26b5f50a6b3ab2074548
S Andrey <Sogomonyan.A.A@sbertech.ru>
2026-02-26 17:41:00 +0300

    salary check

---
+++ src/main/java/multithreading/thread_run/Ex_Volatile.java
@@ -0,0 +1,25 @@
+package multithreading.thread_run;
+
+public class Ex_Volatile extends Thread {
+    volatile boolean b = true;
+
+    public void run() {
+        long counter = 0;
+        while (b) {
+            counter++;
+        }
+        System.out.println("Loop is finished. Loop count: " + counter);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Ex_Volatile exVolatile = new Ex_Volatile();
+        exVolatile.start();
+        Thread.sleep(3000);
+        System.out.println("Afret 3 sec time to wake up!");
+
+        exVolatile.b = false;
+        exVolatile.join();
+
+        System.out.println("_ _ _ F I N I S H _ _ _ ");
+    }
+}
