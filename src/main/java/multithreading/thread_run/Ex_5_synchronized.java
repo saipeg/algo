3cd5a1040a3cef6a1d902ae30ba74819504a80ba
S Andrey <Sogomonyan.A.A@sbertech.ru>
2026-02-26 18:04:49 +0300

    salary check

---
+++ src/main/java/multithreading/thread_run/Ex_5_synchronized.java
@@ -0,0 +1,33 @@
+package multithreading.thread_run;
+
+public class Ex_5_synchronized {
+    public static int counter = 0;
+
+    public static synchronized void increment() { //Без синхронайза расчет будет неверный в рамках работы 2 и более потоков
+        counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread coun1 = new Thread(new Count());
+        Thread coun2 = new Thread(new Count());
+        Thread coun3 = new Thread(new Count());
+        coun1.start();
+        coun2.start();
+        coun3.start();
+        coun1.join();
+        coun2.join();
+        coun3.join();
+        System.out.println(counter);
+    }
+
+}
+
+class Count implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 2000; i ++) {
+            Ex_5_synchronized.increment();
+        }
+    }
+}
